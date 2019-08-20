1ad77eae67b7fd1120f7fae141d6b3a86519cf71
Lachlan Campbell <lachlan@hackclub.com>
2019-08-20 15:47:00 -0400

    Reformat

--- components/showcase/slider/mobile-navigation.js
+++ components/showcase/slider/mobile-navigation.js
@@ -2,39 +2,37 @@ import ShowcaseLink from './showcase-link'
 import ArrowPrevious from '../../icons/arrow-previous'
 import ArrowNext from '../../icons/arrow-next'
 
-function MobileNavigation({ prev, item }) {
-  return (
-    <ShowcaseLink item={item}>
-      <a className={prev ? 'previous' : null}>
-        {prev ? <ArrowPrevious /> : <ArrowNext />}
-        <style jsx>{`
-          a {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            background: #fff;
-            border: none;
-            width: 100%;
-            height: auto;
-            outline: none;
-            cursor: pointer;
-            padding-top: 15px;
-            padding-bottom: 15px;
-            padding-left: 50px;
-            padding-right: 50px;
-          }
+const MobileNavigation = ({ prev, item }) => (
+  <ShowcaseLink item={item}>
+    <a className={prev ? 'previous' : null}>
+      {prev ? <ArrowPrevious /> : <ArrowNext />}
+      <style jsx>{`
+        a {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          background: #fff;
+          border: none;
+          width: 100%;
+          height: auto;
+          outline: none;
+          cursor: pointer;
+          padding-top: 15px;
+          padding-bottom: 15px;
+          padding-left: 50px;
+          padding-right: 50px;
+        }
 
-          a :global(svg) {
-            width: 50px;
-            height: 50px;
-          }
-          a :global(svg) :global(path) {
-            fill: #ec008c !important;
-          }
-        `}</style>
-      </a>
-    </ShowcaseLink>
-  )
-}
+        a :global(svg) {
+          width: 50px;
+          height: 50px;
+        }
+        a :global(svg) :global(path) {
+          fill: #ec008c;
+        }
+      `}</style>
+    </a>
+  </ShowcaseLink>
+)
 
 export default MobileNavigation
--- showcase-manifest.js
+++ showcase-manifest.js
@@ -1,6 +1,151 @@
+const sortOrder = [
+  'cover',
+  'table-of-contents',
+  'manifesto',
+  'consent',
+  'masturbation',
+  'signs-of-unhealthy-relationships-1',
+  'signs-of-unhealthy-relationships-2',
+  'safe-words',
+  'penetration',
+  'penetration-2',
+  'oral-sex-on-penises',
+  'lube-condoms',
+  'condoms',
+  'oral-on-vulvas',
+  'testing',
+  'brain-porn',
+  'check-ins',
+  'body-image',
+  'gender',
+  'sexuality-coming-out',
+  'asexuality-slut-rules',
+  'dating-app-etiquette',
+  'parties',
+  'conclusion'
+]
 
-const sortOrder = ["cover","table-of-contents","manifesto","consent","masturbation","signs-of-unhealthy-relationships-1","signs-of-unhealthy-relationships-2","safe-words","penetration","penetration-2","oral-sex-on-penises","lube-condoms","condoms","oral-on-vulvas","testing","brain-porn","check-ins","body-image","gender","sexuality-coming-out","asexuality-slut-rules","dating-app-etiquette","parties","conclusion"]
-
-const mapping = {"ds-store":{"internalUrl":"ds-store","title":".DS_Store","src":"/static/full_pages/.DS_Store"},"cover":{"internalUrl":"cover","title":"Cover","src":"/static/full_pages/1%20Cover.png"},"penetration-2":{"internalUrl":"penetration-2","title":"Penetration 2","src":"/static/full_pages/10%20Penetration%202.png"},"oral-sex-on-penises":{"internalUrl":"oral-sex-on-penises","title":"Oral Sex on Penises","src":"/static/full_pages/11%20Oral%20Sex%20on%20Penises.png"},"lube-condoms":{"internalUrl":"lube-condoms","title":"Lube + Condoms","src":"/static/full_pages/12%20Lube%20%2B%20Condoms.png"},"condoms":{"internalUrl":"condoms","title":"Condoms","src":"/static/full_pages/13%20Condoms.png"},"oral-on-vulvas":{"internalUrl":"oral-on-vulvas","title":"Oral on Vulvas","src":"/static/full_pages/14%20Oral%20on%20Vulvas.png"},"testing":{"internalUrl":"testing","title":"Testing","src":"/static/full_pages/15%20Testing.png"},"brain-porn":{"internalUrl":"brain-porn","title":"Brain + Porn","src":"/static/full_pages/16%20Brain%20%2B%20Porn.png"},"check-ins":{"internalUrl":"check-ins","title":"Check-ins","src":"/static/full_pages/17%20Check-ins.png"},"body-image":{"internalUrl":"body-image","title":"Body Image","src":"/static/full_pages/18%20Body%20Image.png"},"gender":{"internalUrl":"gender","title":"Gender","src":"/static/full_pages/19%20Gender.png"},"table-of-contents":{"internalUrl":"table-of-contents","title":"Table of Contents","src":"/static/full_pages/2%20Table%20of%20Contents.png"},"sexuality-coming-out":{"internalUrl":"sexuality-coming-out","title":"Sexuality + Coming Out","src":"/static/full_pages/20%20Sexuality%20%2B%20Coming%20Out.png"},"asexuality-slut-rules":{"internalUrl":"asexuality-slut-rules","title":"Asexuality + Slut Rules","src":"/static/full_pages/21%20Asexuality%20%2B%20Slut%20Rules.png"},"dating-app-etiquette":{"internalUrl":"dating-app-etiquette","title":"Dating App Etiquette","src":"/static/full_pages/22%20Dating%20App%20Etiquette.png"},"parties":{"internalUrl":"parties","title":"Parties","src":"/static/full_pages/23%20Parties.png"},"conclusion":{"internalUrl":"conclusion","title":"Conclusion","src":"/static/full_pages/24%20Conclusion.png"},"manifesto":{"internalUrl":"manifesto","title":"Manifesto","src":"/static/full_pages/3%20Manifesto.png"},"consent":{"internalUrl":"consent","title":"Consent","src":"/static/full_pages/4%20Consent.png"},"masturbation":{"internalUrl":"masturbation","title":"Masturbation","src":"/static/full_pages/5%20Masturbation.png"},"signs-of-unhealthy-relationships-1":{"internalUrl":"signs-of-unhealthy-relationships-1","title":"Signs of Unhealthy Relationships 1","src":"/static/full_pages/6%20Signs%20of%20Unhealthy%20Relationships%201.png"},"signs-of-unhealthy-relationships-2":{"internalUrl":"signs-of-unhealthy-relationships-2","title":"Signs of Unhealthy Relationships 2","src":"/static/full_pages/7%20Signs%20of%20Unhealthy%20Relationships%202.png"},"safe-words":{"internalUrl":"safe-words","title":"Safe Words","src":"/static/full_pages/8%20Safe%20Words.png"},"penetration":{"internalUrl":"penetration","title":"Penetration","src":"/static/full_pages/9%20Penetration.png"}}
+const mapping = {
+  cover: {
+    internalUrl: 'cover',
+    title: 'Cover',
+    src: '/static/full_pages/1%20Cover.png'
+  },
+  'penetration-2': {
+    internalUrl: 'penetration-2',
+    title: 'Penetration 2',
+    src: '/static/full_pages/10%20Penetration%202.png'
+  },
+  'oral-sex-on-penises': {
+    internalUrl: 'oral-sex-on-penises',
+    title: 'Oral Sex on Penises',
+    src: '/static/full_pages/11%20Oral%20Sex%20on%20Penises.png'
+  },
+  'lube-condoms': {
+    internalUrl: 'lube-condoms',
+    title: 'Lube + Condoms',
+    src: '/static/full_pages/12%20Lube%20%2B%20Condoms.png'
+  },
+  condoms: {
+    internalUrl: 'condoms',
+    title: 'Condoms',
+    src: '/static/full_pages/13%20Condoms.png'
+  },
+  'oral-on-vulvas': {
+    internalUrl: 'oral-on-vulvas',
+    title: 'Oral on Vulvas',
+    src: '/static/full_pages/14%20Oral%20on%20Vulvas.png'
+  },
+  testing: {
+    internalUrl: 'testing',
+    title: 'Testing',
+    src: '/static/full_pages/15%20Testing.png'
+  },
+  'brain-porn': {
+    internalUrl: 'brain-porn',
+    title: 'Brain + Porn',
+    src: '/static/full_pages/16%20Brain%20%2B%20Porn.png'
+  },
+  'check-ins': {
+    internalUrl: 'check-ins',
+    title: 'Check-ins',
+    src: '/static/full_pages/17%20Check-ins.png'
+  },
+  'body-image': {
+    internalUrl: 'body-image',
+    title: 'Body Image',
+    src: '/static/full_pages/18%20Body%20Image.png'
+  },
+  gender: {
+    internalUrl: 'gender',
+    title: 'Gender',
+    src: '/static/full_pages/19%20Gender.png'
+  },
+  'table-of-contents': {
+    internalUrl: 'table-of-contents',
+    title: 'Table of Contents',
+    src: '/static/full_pages/2%20Table%20of%20Contents.png'
+  },
+  'sexuality-coming-out': {
+    internalUrl: 'sexuality-coming-out',
+    title: 'Sexuality + Coming Out',
+    src: '/static/full_pages/20%20Sexuality%20%2B%20Coming%20Out.png'
+  },
+  'asexuality-slut-rules': {
+    internalUrl: 'asexuality-slut-rules',
+    title: 'Asexuality + Slut Rules',
+    src: '/static/full_pages/21%20Asexuality%20%2B%20Slut%20Rules.png'
+  },
+  'dating-app-etiquette': {
+    internalUrl: 'dating-app-etiquette',
+    title: 'Dating App Etiquette',
+    src: '/static/full_pages/22%20Dating%20App%20Etiquette.png'
+  },
+  parties: {
+    internalUrl: 'parties',
+    title: 'Parties',
+    src: '/static/full_pages/23%20Parties.png'
+  },
+  conclusion: {
+    internalUrl: 'conclusion',
+    title: 'Conclusion',
+    src: '/static/full_pages/24%20Conclusion.png'
+  },
+  manifesto: {
+    internalUrl: 'manifesto',
+    title: 'Manifesto',
+    src: '/static/full_pages/3%20Manifesto.png'
+  },
+  consent: {
+    internalUrl: 'consent',
+    title: 'Consent',
+    src: '/static/full_pages/4%20Consent.png'
+  },
+  masturbation: {
+    internalUrl: 'masturbation',
+    title: 'Masturbation',
+    src: '/static/full_pages/5%20Masturbation.png'
+  },
+  'signs-of-unhealthy-relationships-1': {
+    internalUrl: 'signs-of-unhealthy-relationships-1',
+    title: 'Signs of Unhealthy Relationships 1',
+    src: '/static/full_pages/6%20Signs%20of%20Unhealthy%20Relationships%201.png'
+  },
+  'signs-of-unhealthy-relationships-2': {
+    internalUrl: 'signs-of-unhealthy-relationships-2',
+    title: 'Signs of Unhealthy Relationships 2',
+    src: '/static/full_pages/7%20Signs%20of%20Unhealthy%20Relationships%202.png'
+  },
+  'safe-words': {
+    internalUrl: 'safe-words',
+    title: 'Safe Words',
+    src: '/static/full_pages/8%20Safe%20Words.png'
+  },
+  penetration: {
+    internalUrl: 'penetration',
+    title: 'Penetration',
+    src: '/static/full_pages/9%20Penetration.png'
+  }
+}
 
 module.exports = { sortOrder, mapping }
